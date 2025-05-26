dcc41c0706833cf779f737e74a1ef85139b3720d
Naveed Ahmed <naveedahmed10001@gmail.com>
2025-05-26 17:39:20 +0500

    Validation Changed

--- roles/rolesValidator.js
+++ roles/rolesValidator.js
@@ -2,9 +2,8 @@ const Joi = require('joi');
 
 exports.validateCreateRole = (data) => {
     const schema = Joi.object({
-        role_name: Joi.string().required().max(100).messages({
+        role_name: Joi.string().required().messages({
             'string.empty': 'Role name is required',
-            'string.max': 'Role name must not exceed 100 characters'
         }),
     });
     return schema.validate(data, { abortEarly: false });
@@ -12,9 +11,8 @@ exports.validateCreateRole = (data) => {
 
 exports.validateUpdateRole = (data) => {
     const schema = Joi.object({
-        role_name: Joi.string().required().max(100).messages({
+        role_name: Joi.string().required().messages({
             'string.empty': 'Role name is required',
-            'string.max': 'Role name must not exceed 100 characters'
         }),
     });
     return schema.validate(data, { abortEarly: false });
